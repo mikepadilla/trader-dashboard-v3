3d121339895856e511e37b3a633fb8c0b9dbfee6
mikepadilla <mikepadilla1@gmail.com>
2025-02-04 22:31:33 -0500

    Update LineChart.tsx

--- src/components/chart/LineChart.tsx
+++ src/components/chart/LineChart.tsx
@@ -136,6 +136,8 @@ const LineChart = ({ min, max, chartDataProp, yKey, events }) => {
   }
 
   const options: NewChartOptionLine = {
+    maintainAspectRatio: true,
+    aspectRatio: 3,
     animation: false,
     layout: {
       padding: 0,
