c4a238ab3335022390dc5efbab8f9c64b05196d0
mikepadilla <mikepadilla1@gmail.com>
2025-02-04 22:48:51 -0500

    Update BarChart.tsx

--- src/components/chart/BarChart.tsx
+++ src/components/chart/BarChart.tsx
@@ -125,7 +125,7 @@ const BarChart = ({ chartDataProp, yKey, events }) => {
 
   const options: NewChartOptionBar = {
     maintainAspectRatio: true,
-    aspectRatio: 2.8,     
+    aspectRatio: 2.7,     
     animation: false,
     layout: {
       padding: 0,
